f461e029de0ca9ff73d9cc5e03d137b9128faa68
sunlearnit <pd818kv@gmail.com>
2025-11-13 18:11:43 +0700

    thêm policy, ..

---
+++ my-app1/src/app/pages/policy/policy.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-policy',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './policy.html',
+  styleUrl: './policy.css'
+})
+export class Policy {
+
+}
