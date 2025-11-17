8280663c83feca47c230e7f4e993ace1048bbda9
LeNhat <nhatltd23411@st.uel.edu.vn>
2025-11-17 13:51:58 +0700

    chỉnh sửa lại cho đẹp

--- my-app1/src/app/pages/contact/contact.ts
+++ my-app1/src/app/pages/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +7,16 @@ import { Component } from '@angular/core';
 })
 export class Contact {
   isChatVisible: boolean = false;
+  public showBackToTop: boolean = false;  
 
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    // Lấy vị trí cuộn (tương thích nhiều trình duyệt)
+    const scrollOffset = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // Chỉ hiện nút nếu người dùng cuộn xuống quá 300px
+    this.showBackToTop = scrollOffset > 300;
+  }
   toggleChat() {
     this.isChatVisible = !this.isChatVisible;
   }
